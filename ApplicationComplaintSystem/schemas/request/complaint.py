71a1a2c0c9d77b4e43c2135292d3668d1c184b23
Lyudmila Sokolova <81382473+liusska@users.noreply.github.com>
2022-05-11 12:08:21 +0300

    Delete ApplicationComplaintSystem directory

--- ApplicationComplaintSystem/schemas/request/complaint.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import Schema, fields, validate
-
-
-class ComplaintCreateRequestSchema(Schema):
-    title = fields.String(required=True, validate=validate.Length(max=100))
-    description = fields.String(required=True, validate=validate.Length(max=100))
-    photo_url = fields.String(required=True, validate=validate.Length(max=100))
-    amount = fields.Float(required=True)
